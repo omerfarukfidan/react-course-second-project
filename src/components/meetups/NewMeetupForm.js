2426b7ecda63c0113912ed4c28843b11ad9fd6ab
omerfarukfidan <fidanomerfaruk01@gmail.com>
2023-07-16 18:31:59 +0300

    Firebase integrated and POST method works with it

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
   const titleInputRef = useRef();
   const imageInputRef = useRef();
   const addressInputRef = useRef();
@@ -23,7 +23,7 @@ function NewMeetupForm() {
         descrition: enteredDescription,
     };
 
-    console.log(meetupData);
+    props.onAddMeetup(meetupData);
   }
 
   return (
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -2,10 +2,23 @@ import React from "react";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetUpPage() {
+  function addMeetupHandler(meetupData) {
+    fetch(
+      'https://react-getting-started-c25dc-default-rtdb.europe-west1.firebasedatabase.app/meetups.json',
+      {
+        method: 'POST', 
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
