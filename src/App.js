8a7a01d841c21e829e1dacba434bafc97943fc72
omerfarukfidan <fidanomerfaruk01@gmail.com>
2023-07-16 16:41:41 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { Route, Switch } from "react-router-dom";
+import React from "react";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetUpPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import Layout from "./components/layout/Layout";
+
+function App() {
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetUpPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </Layout>
+  );
+}
+
+export default App;
---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewMeetUpPage() {
+  return <section>
+    <h1>Add New Meetup</h1>
+    <NewMeetUpPage />
+  </section>
+
+}
+
+export default NewMeetUpPage;
