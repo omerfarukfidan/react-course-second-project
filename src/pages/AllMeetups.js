f1581c3f881ba5ab4976f377e6ef492426a476c8
omerfarukfidan <fidanomerfaruk01@gmail.com>
2023-07-18 15:12:26 +0300

    end

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -2,29 +2,6 @@ import React from "react";
 import { useState, useEffect } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
-/*
-const DUMMY_DATA = [
-  {
-    id: "m1",
-    title: "This is a first meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-  {
-    id: "m2",
-    title: "This is a second meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-];
-*/
-
 function AllMeetupsPage() {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedMeetup, setLoadedMeetups] = useState([]);
@@ -40,10 +17,10 @@ function AllMeetupsPage() {
       .then((data) => {
         const meetups = [];
 
-        for (const key in data){
-          const meetup ={
-              id: key,
-              ...data[key]
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
           };
 
           meetups.push(meetup);
